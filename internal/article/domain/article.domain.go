22f9376b3d6b2bc9bde4b7dce0739aeeabe86294
chien.cq <caochientp1@gmail.com>
2024-07-10 00:03:17 +0700

    chore: Update folder structure

--- internal/article/domain/article.domain.go
+++ internal/article/domain/article.domain.go
@@ -1,12 +1,15 @@
 package domain
 
-import "time"
+import (
+	"time"
+	authorDomain "todo/internal/author/domain"
+)
 
 type Article struct {
-	ID        int64     `json:"id"`
-	Title     string    `json:"title" validate:"required"`
-	Content   string    `json:"content" validate:"required"`
-	Author		Author    `json:"author"`
-	UpdatedAt time.Time `json:"updated_at"`
-	CreatedAt time.Time `json:"created_at"`
+	ID        int64               `json:"id"`
+	Title     string              `json:"title" validate:"required"`
+	Content   string              `json:"content" validate:"required"`
+	Author    authorDomain.Author `json:"author"`
+	UpdatedAt time.Time           `json:"updated_at"`
+	CreatedAt time.Time           `json:"created_at"`
 }
